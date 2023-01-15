710ce930ec50876447c844754e324c533247405d
suzhiwei2000 <83455164+suzhiwei2000@users.noreply.github.com>
2023-01-15 21:50:49 +0800

    提交首页封面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="home card">
-		<img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
+		<Lottie width="100%" height="100%" class="Lottie" src="https://assets3.lottiefiles.com/packages/lf20_puciaact.json" />
 	</div>
 </template>
 
